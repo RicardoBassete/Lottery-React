2aa23d142d5738062f67fcb37a7f18092fe2da3a
Ricardo Gonçalves Bassete <ricardogbassete@gmail.com>
2022-05-13 17:06:55 -0300

    Update app.js

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
 
 import web3 from './web3.js'
+import lottery from './lottery'
+import { useEffect, useState } from 'react';
 
 function App() {
+
+  const [manager, setManager] = useState('')
+  const [players, setPlayers] = useState([])
+  const [balance, setBalance] = useState('')
+  const [value, setValue] = useState('')
+
+  useEffect(() => {
+    const start = async () => {
+      const manager = await lottery.methods.manager().call()
+      const players = await lottery.methods.getPlayers().call()
+      const balance = await web3.eth.getBalance(lottery.options.address)
+
+      setManager(manager)
+      setPlayers(players)
+      setBalance(balance)
+    }
+    start()    
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h2>Lottery Contract</h2>
+      <p>This contrtact is managed by {manager}</p>
+      <p>There are currently {players.length} people in the lottery competing for {web3.utils.fromWei(balance, 'ether')} ether</p>
+      <hr />
+      <form>
+        <h4>Want to try your luck?</h4>
+        <div>
+          <label>Amount of ether to enter </label>
+          <input 
+            onChange={event => setValue(event.target.value)}
+            value={value}
+          />
+        </div>
+        <button>Enter</button>
+      </form>
     </div>
+      
   );
 }
 
